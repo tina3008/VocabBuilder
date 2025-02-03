3428c06302250e0ec1a2aec1a5856054b973dcdf
tina3008 <kicja_3@ukr.net>
2025-02-03 11:41:07 +0200

    pagination320

--- src/components/PaginatedItems/PaginatedItems.jsx
+++ src/components/PaginatedItems/PaginatedItems.jsx
@@ -1,55 +1,7 @@
 import React, { useEffect, useState } from "react";
 import ReactPaginate from "react-paginate";
-import { fetchWordsOwn } from "../../redux/words/operations";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import css from "./PaginatedItems.module.css";
-
-// export const PaginatedItems = () => {
-//   const dispatch = useDispatch();
-//   const { totalPage, currentPage } = useSelector((state) => state.words);
-//   const [page, setPage] = useState(0);
-
-//  const itemsPerPage=3
-//   useEffect(() => {
-//     if (currentPage !== page + 1) {
-//       dispatch(fetchWordsOwn(page + 1));
-//     }
-//   }, [page, dispatch, currentPage]);
-
-//   const handlePageClick = ({ selected }) => {
-//     setPage(selected);
-//   };
-
-//   const pageCount = totalPage || 1;
-
-//   return (
-//     <div className={css.paginationBlock}>
-//       <button onClick={() => setPage(0)} className={css.pageButton}>
-//         &lt;&lt;
-//       </button>
-//       <ReactPaginate
-//         className={css.pagination}
-//         pageClassName={css.pageClassName}
-//         activeClassName={css.activeClassName}
-//         previousClassName={css.previousClassName}
-//         nextClassName={css.nextClassName}
-//         breakLabel="..."
-//         nextLabel=">"
-//         onPageChange={handlePageClick}
-//         pageRangeDisplayed={3}
-//         marginPagesDisplayed={1}
-//         pageCount={pageCount}
-//         previousLabel="<"
-//         renderOnZeroPageCount={null}
-//         forcePage={page} 
-//       />
-//       <button onClick={() => setPage(pageCount - 1)} className={css.pageButton}>
-//         &gt;&gt;
-//       </button>
-//     </div>
-//   );
-// };
-
 
 export const PaginatedItems = ({
   items,
@@ -59,7 +11,7 @@ export const PaginatedItems = ({
 }) => {
   const dispatch = useDispatch();
   const [page, setPage] = useState(currentPage - 1);
-
+  const [pageRange, setPageRange] = useState(3);
   useEffect(() => {
     if (currentPage !== page + 1) {
       dispatch(fetchAction(page + 1));
@@ -71,6 +23,17 @@ export const PaginatedItems = ({
   };
 
   const pageCount = totalPage || 1;
+
+    useEffect(() => {
+      const updatePageRange = () => {
+        setPageRange(window.innerWidth < 768 ? 2 : 3);
+      };
+
+      updatePageRange(); 
+      window.addEventListener("resize", updatePageRange);
+
+      return () => window.removeEventListener("resize", updatePageRange);
+    }, []);
 
   return (
     <div className={css.paginationBlock}>
@@ -86,7 +49,7 @@ export const PaginatedItems = ({
         breakLabel="..."
         nextLabel=">"
         onPageChange={handlePageClick}
-        pageRangeDisplayed={3}
+        pageRangeDisplayed={pageRange}
         marginPagesDisplayed={1}
         pageCount={pageCount}
         previousLabel="<"
